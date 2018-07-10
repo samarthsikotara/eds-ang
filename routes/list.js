ea1a6ab6400d193e976f52e08a2e7ed0917d986b
samarth sikotara <samarthsikotara@gmail.com>
2018-07-11 01:10:26 +0530

    adding repo instructions

--- routes/list.js
+++ routes/list.js
@@ -1,7 +1,7 @@
 module.exports = [
-	{method: "GET", path: "/api/categories", name: "Show categories", controller: "api/category", action: "index"},
-	{method: "POST", path: "/api/categories", name: "Show main", controller: "api/category", action: "create"},
-	{method: "GET", path: "/api/categories/:categoryId", name: "Show categories", controller: "api/category", action: "show"},
+	{method: "GET", path: "/api/categories", name: "Show Categories", controller: "api/category", action: "index"},
+	{method: "POST", path: "/api/categories", name: "Create Categories", controller: "api/category", action: "create"},
+	{method: "GET", path: "/api/categories/:categoryId", name: "Show Categories", controller: "api/category", action: "show"},
 	{method: "GET", path: "/api/categories/:categoryId/products", name: "Show categorized products", controller: "api/category", action: "products"},
 	{method: "POST", path: "/api/products", name: "Create Product", controller: "api/product", action: "create"},
 	{method: "POST", path: "/api/products/:productId/update", name: "Update Product", controller: "api/product", action: "update"}
